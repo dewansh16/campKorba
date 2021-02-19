38910de00b2348a5d5563020dc7baa602d469e6a
Dewansh <dewanshpatle69@gmail.com>
2021-02-19 19:44:25 +0530

    adding styles

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
-const { findByIdAndDelete } = require('./models/campground');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -19,6 +20,7 @@ db.once('open', function () {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
